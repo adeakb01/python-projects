964b1ae7b50b4d64be3e7d328e8c7537c10aabd8
Adeeba <adeebaakbar01@gmail.com>
2025-09-22 00:27:58 +0530

    Update gif.py

--- gif.py
+++ gif.py
@@ -2,7 +2,7 @@ import imageio.v3 as iio
 from PIL import Image
 import numpy as np
 
-filenames=['cat2.jpeg','cat1.jpg']
+filenames=['cat-1.jpeg','cat-2.jpeg']
 images=[]
 
 target_size=Image.open(filenames[0]).size
@@ -10,4 +10,5 @@ target_size=Image.open(filenames[0]).size
 for filename in filenames:
     img=Image.open(filename).resize(target_size)
     images.append(np.array(img))
-iio.imwrite('cat2.gif', images, duration=500,loop=0)
+
+iio.imwrite('cat.gif', images, duration=500,loop=0)
